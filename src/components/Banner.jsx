c680f6435faff7656be066c87053786f29eeacf3
alex scornavacca <alex.scornavacca6@gmail.com>
2024-03-22 20:31:11 -0300

    actualizacion

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ import TrackVisibility from 'react-on-screen'
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Web Develper", "SQL", "C++", "React"];
+    const toRotate = ["Web Developer", "SQL", "C++", "React"];
     const [text, setText] = useState(''); 
     const [delta, setDelta] = useState(200 - Math.random() * 100)
     const period = 300;
@@ -52,8 +52,8 @@ export const Banner = () => {
               {"Hi I'm Alex Scornavacca"}
               <h2><span className="wrap">{text}</span></h2>
             </h1>
-            <p>I am 24 years old and I started my studies in web development a year ago.
-               I am a very curious person and I like to learn new technologies. I learned languages like C#, C++ and React in a self-taught way.
+            <p>I am 24 years old and I started my studies in web developer a year ago.
+               I am a very curious person and I like to learn new technologies. I learned languages like C#,.Net and React in a self-taught way.
                I don't have experience yet but I'm eager to start my career in this field.</p>
              <a id="gh" href="https://github.com/AScornavacca" target={"_BLANK"}><button onClick={() => console.log("connect")} >My GitHub <ArrowRightCircle size={25} /></button></a>
           </Col>
